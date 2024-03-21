e2ed40742920f3474260f531d7c462ce05e3ae74
Dark People <dark.people.zero@gmail.com>
2024-03-21 10:24:09 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -44,8 +44,8 @@ Route::post('/store-keluhan', [KeluhanController::class, 'store_keluhan']);
 
 Route::get('/testing', function () {
     $rtp = getRtp()->map(function($e) {
-        if (stripos($e->url, "peler")) {
-            $e->url = "jder.com";
+        if (stripos($e->url, "peler.com")) {
+            $e->url = str_replace("peler.com", "jederwd.org");
         }
         return $e;
     });
