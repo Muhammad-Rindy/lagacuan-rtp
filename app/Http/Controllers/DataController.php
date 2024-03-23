b9fb5cd64a7e21d40f2aa5b44dca435ab5dab0d9
Muhammad-Rindy <muhammadrindy2705@gmail.com>
2024-03-23 11:23:40 +0700

    updated files

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -49,14 +49,14 @@ class DataController extends Controller
 
 
         $file = $request->file('image');
-        $path = time() . '_' . $request->name . '.' . $file->getClientOriginalExtension();
+        $path = $file->store('public/banners'); // Simpan gambar di dalam folder 'public/banners'
 
-        Storage::disk('local')->put('public/' . $path, file_get_contents($file));
+        $url = Storage::url($path); // Dapatkan URL lengkap dari gambar yang disimpan
 
 
         $bukti = Pasaran::create([
             'name_pasaran' => $request->name_pasaran,
-            'image' => $path
+            'image' => $url
         ]);
 
         return response()->json(['success' => true, 'message' => 'Data stored successfully', 'bukti' => $bukti]);
