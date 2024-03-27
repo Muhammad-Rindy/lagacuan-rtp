86eeb98c316ab5fa42fff9a54994c69b67b2f0fc
Muhammad-Rindy <muhammadrindy2705@gmail.com>
2024-03-27 20:01:25 +0700

    updated

--- resources/views/rtpslot_page.blade.php
+++ resources/views/rtpslot_page.blade.php
@@ -83,10 +83,17 @@
             padding-bottom: 3px;
         }
 
+        .height-card {
+            height: 150px;
+        }
 
     }
 
     @media screen and (max-width: 900px) {
+
+        .height-card {
+            height: 101px;
+        }
 
         .provider-game {
             background-image: linear-gradient(132deg, #fbf475 0%, #c3b60c 85%);
@@ -238,7 +245,7 @@
                             <div class="col-4 data_rtp" style="padding: 7px;">
                                 <div class="shadow card h-100 lazy-image card-rtp-new" style="color: white;border: solid #198cf5 3px">
                                     <a href="${e.url}" class="provider-game">
-                                        <img src="${e.image}" class="card-img-top img-new" alt="..." style="height: 101px;">
+                                        <img src="${e.image}" class="card-img-top img-new height-card" alt="Gambar">
                                     </a>
                                     <div class="mt-2 progress bar-persen">
                                         <div class="progress-bar ${
