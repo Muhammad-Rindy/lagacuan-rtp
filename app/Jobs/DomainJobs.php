38d229d249a337b1e416f36ba8d7b8b91a77dec7
Dark People <dark.people.zero@gmail.com>
2024-05-01 17:40:00 +0700

    udpate

--- app/Jobs/DomainJobs.php
+++ app/Jobs/DomainJobs.php
@@ -55,7 +55,7 @@ class DomainJobs implements ShouldQueue
             $this->files->put($path, $contents);
         }
 
-        shell_exec('sudo systemctl restart nginx');
+        shell_exec('sudo -S systemctl restart nginx');
     }
 
     /**
