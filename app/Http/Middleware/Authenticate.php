115856af746db4cdb4ad82d05aa0c8cc70e6fd3c
Muhammad Rindy <muhammadrindy2705@gmail.com>
2025-03-01 15:04:28 +0700

    update_files

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -14,8 +14,8 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
-        if (! $request->expectsJson()) {
-            return route('index-pasaran-home');
+        if (!$request->expectsJson()) {
+            return route('index-rtp');
         }
     }
 }
